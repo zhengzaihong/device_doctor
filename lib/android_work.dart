adc0a514754ce664ada8d742639ab4f37d6222ab
zhengzaihong <1096877329@qq.com>
2024-03-02 20:55:23 +0800

    "--"

--- lib/android_work.dart
+++ lib/android_work.dart
@@ -17,54 +17,54 @@ class AndroidWork {
   final MethodChannel _channel;
 
   // ///获取Android运行的进程
-  // Future<dynamic> getRunningAppProcesses() async => await _channel.invokeMethod('getRunningAppProcesses');
+  // Future<dynamic> getRunningAppProcesses() async =>  _channel.invokeMethod('getRunningAppProcesses');
   //
   // ///检查当前设备是否是模拟器 返回值大于3 基本就可以断定为 模拟器了
-  // Future<dynamic> isEmulator() async => await _channel.invokeMethod('checkDeviceIsEmulator');
+  // Future<dynamic> isEmulator() async =>  _channel.invokeMethod('checkDeviceIsEmulator');
 
 
   /// 获取Android版本
   Future<dynamic> get platformVersion async {
-    return await _channel.invokeMethod('getPlatformVersion');
+    return  _channel.invokeMethod('getPlatformVersion');
   }
 
   /// 获取设备唯一标识
    Future<dynamic> get deviceIMEINumber async {
-    return await _channel.invokeMethod("getIMEINumber");
+    return  _channel.invokeMethod("getIMEINumber");
   }
 
   /// 获取设备API级
    Future<dynamic> get apiLevel async {
-    return await _channel.invokeMethod("getAPILevel");
+    return  _channel.invokeMethod("getAPILevel");
   }
 
   /// 获取设备型号
    Future<dynamic> get deviceModel async {
-    return await _channel.invokeMethod("getModel");
+    return  _channel.invokeMethod("getModel");
   }
 
   /// 获取设备厂商
    Future<dynamic> get deviceManufacturer async {
-    return await _channel.invokeMethod("getManufacturer");
+    return  _channel.invokeMethod("getManufacturer");
   }
 
   /// 获取设备名称
    Future<dynamic> get deviceName async {
-    return  await _channel.invokeMethod("getDevice");
+    return  _channel.invokeMethod("getDevice");
   }
 
   /// 获取产品名称
    Future<dynamic> get productName async {
-    return await _channel.invokeMethod("getProduct");
+    return _channel.invokeMethod("getProduct");
   }
 
   /// 获取CPU名称
    Future<dynamic> get cpuName async {
-    return await _channel.invokeMethod("getCPUType");
+    return  _channel.invokeMethod("getCPUType");
   }
 
   /// 获取硬件
    Future<dynamic> get hardware async {
-    return await _channel.invokeMethod("getHardware");
+    return  _channel.invokeMethod("getHardware");
   }
 }
